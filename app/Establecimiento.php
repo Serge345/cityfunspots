3ebe8b92ef958e0bda6ce9c72259eb1ce49c58ac
Serge345 <sergio.salazarw@autonoma.edu.co>
2015-09-18 13:30:05 -0500

    Crear nuevo usuario listo,problemas con la muestra de errores

--- app/Establecimiento.php
+++ app/Establecimiento.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Establecimientos extends Model
+class Establecimiento extends Model
 {
     //
 }
--- app/Publicacion.php
+++ app/Publicacion.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Publicaciones extends Model
+class Publicacion extends Model
 {
     //
 }
